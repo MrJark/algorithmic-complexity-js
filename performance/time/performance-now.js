c1452f0302fc53d421e0dcbbfcc4a077d36937ae
MrJark <mrjark7@gmail.com>
2023-03-07 07:04:36 +0100

    've added the first algorithm. A function

---
+++ performance/time/performance-now.js
@@ -0,0 +1,12 @@
+function count(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);        
+    }
+}
+
+let initial_time = performance.now();
+count(57);
+let final_time = performance.now();
+let duration = final_time - initial_time;
+
+console.log(`El tiempo que ha durado el algoritmo ha sido ${duration}`);
